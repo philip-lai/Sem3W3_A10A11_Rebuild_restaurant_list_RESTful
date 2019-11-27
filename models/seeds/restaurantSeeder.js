8e36489001a9479aabef8df58ff9a545aa1cfd55
philip_lai <pupuhouse@yahoo.com.tw>
2019-11-27 22:52:21 +0800

    add README_pic

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const User = require('../user')
 const bcrypt = require('bcryptjs')
-const restaurant = require('../restaurant.json').results
-const users = require('../users.json').users
+const restaurantlist = require('../restaurant.json').results
+const userlist = require('../users.json').users
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -19,27 +19,27 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('db connected')
-  for (let i = 0; i < users.length; i++) {
+  for (let i = 0; i < userlist.length; i++) {
     bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(users[i].password, salt, (err, hash) => {
+      bcrypt.hash(userlist[i].password, salt, (err, hash) => {
         const newUser = new User({
-          name: users[i].name,
-          email: users[i].email,
+          name: userlist[i].name,
+          email: userlist[i].email,
           password: hash
         })
         newUser.save().then(user => {
           // create restaurant data for each user
           for (let j = i * 3; j < (i + 1) * 3; j++) {
             Restaurant.create({
-              name: restaurant[j].name,
-              name_en: restaurant[j].name_en,
-              category: restaurant[j].category,
-              image: restaurant[j].image,
-              location: restaurant[j].location,
-              phone: restaurant[j].phone,
-              google_map: restaurant[j].google_map,
-              rating: restaurant[j].rating,
-              description: restaurant[j].description,
+              name: restaurantlist[j].name,
+              name_en: restaurantlist[j].name_en,
+              category: restaurantlist[j].category,
+              image: restaurantlist[j].image,
+              location: restaurantlist[j].location,
+              phone: restaurantlist[j].phone,
+              google_map: restaurantlist[j].google_map,
+              rating: restaurantlist[j].rating,
+              description: restaurantlist[j].description,
               userId: user._id
             })
           }
